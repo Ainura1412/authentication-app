ca881bf9bedaca0fa7ea005008f1fafa95dae7ac
Ainura <abekbolsunova@gmail.com>
2022-04-04 22:31:05 +0600

    FirebaseAuth-app

---
+++ src/firebase/configFirebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore'
+const firebaseConfig = {
+    apiKey: "AIzaSyBvhKmg9S10kLOXD2U9nsAGelGB5FD2pHo",
+    authDomain: "reactfirebse-b3136.firebaseapp.com",
+    projectId: "reactfirebse-b3136",
+    storageBucket: "reactfirebse-b3136.appspot.com",
+    messagingSenderId: "760429492045",
+    appId: "1:760429492045:web:8658f3997f83369fe36904",
+    measurementId: "G-WHJBDWJC60"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const firestore = getFirestore(app)
